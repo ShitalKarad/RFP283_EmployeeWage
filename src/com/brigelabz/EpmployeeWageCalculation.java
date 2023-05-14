94d4c4e714f0cb653ba891839be262b5a93e7b5b
ShitalKarad <shitalkarad24@gmail.com>
2023-05-14 18:26:02 +0530

    Added:UC2_EmpWage

--- src/com/brigelabz/EpmployeeWageCalculation.java
+++ src/com/brigelabz/EpmployeeWageCalculation.java
@@ -4,13 +4,18 @@ public class EpmployeeWageCalculation {
 	public static void main(String[] args) {
 		int present = 1;
 		int upsent = 0;
+	    final int wage_per_hour = 20;
+	    final int full_day_hour = 8;
 		int checkRandom = (int) (Math.floor(Math.random() * 10)%2);
-		
+		int totalWage = 0;
 		if (checkRandom == 1) {
-			System.out.println("Employee is present");
+			totalWage = wage_per_hour * full_day_hour;
+			System.out.println("Employee is present "+totalWage);
 		}
 		else {
-			System.out.println("Employee is upsent");
+			
+			System.out.println("Employee is upsent " +totalWage);
+			
 		}
 		
 	}
